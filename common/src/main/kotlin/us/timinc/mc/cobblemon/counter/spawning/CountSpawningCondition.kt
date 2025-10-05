32ddd179933698d604b9ac7873a05652d69c6890
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-10-05 00:01:01 -0700

    * bumped tim core to latest. * Added counter-based spawning condition.

---
+++ common/src/main/kotlin/us/timinc/mc/cobblemon/counter/spawning/CountSpawningCondition.kt
@@ -0,0 +1,28 @@
+package us.timinc.mc.cobblemon.counter.spawning
+
+import com.cobblemon.mod.common.api.spawning.condition.AppendageCondition
+import com.cobblemon.mod.common.api.spawning.context.SpawningContext
+import net.minecraft.server.level.ServerPlayer
+import us.timinc.mc.cobblemon.counter.extension.getCounterManager
+
+class CountSpawningCondition : AppendageCondition {
+    @Suppress("MemberVisibilityCanBePrivate")
+    val counts: List<CountRequirement>? = null
+    @Suppress("MemberVisibilityCanBePrivate")
+    val streaks: List<CountRequirement>? = null
+
+    override fun fits(ctx: SpawningContext): Boolean {
+        val player = ctx.cause.entity as? ServerPlayer ?: return true
+        val manager = player.getCounterManager()
+
+        if (streaks !== null) streaks.forEach { req ->
+            if (manager.getStreakScore(req.type, req.speciesRl, req.form) < req.amount) return false
+        }
+
+        if (counts !== null) counts.forEach { req ->
+            if (manager.getCountScore(req.type, req.speciesRl, req.form) < req.amount) return false
+        }
+
+        return true
+    }
+}
