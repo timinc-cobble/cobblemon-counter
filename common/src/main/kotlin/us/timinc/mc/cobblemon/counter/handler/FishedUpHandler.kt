c35886cb5fd4f73538fcd12f32954f1db184ea06
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-11-08 15:18:19 -0800

    Named the fished up handler better and utilized the new POKEMON_ENTITY_DID_SPAWN event.

--- common/src/main/kotlin/us/timinc/mc/cobblemon/counter/handler/FishedUpHandler.kt
+++ common/src/main/kotlin/us/timinc/mc/cobblemon/counter/handler/FishedUpHandler.kt
@@ -1,22 +1,22 @@
 package us.timinc.mc.cobblemon.counter.handler
 
-import com.cobblemon.mod.common.api.events.entity.SpawnEvent
 import com.cobblemon.mod.common.api.spawning.context.FishingSpawningContext
 import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
 import net.minecraft.server.level.ServerPlayer
 import us.timinc.mc.cobblemon.counter.CounterMod.CounterTypes
 import us.timinc.mc.cobblemon.counter.extension.record
+import us.timinc.mc.cobblemon.timcore.AbstractHandler
+import us.timinc.mc.cobblemon.timcore.event.EntityDidSpawnEvent
 
-object PokemonEntitySpawnHandler {
-    fun handle(spawnEvent: SpawnEvent<PokemonEntity>) {
-        val ctx = spawnEvent.ctx
+object FishedUpHandler : AbstractHandler<EntityDidSpawnEvent<PokemonEntity>>() {
+    override fun handle(evt: EntityDidSpawnEvent<PokemonEntity>) {
+        val ctx = evt.ctx
         if (ctx !is FishingSpawningContext) return
 
         val player = ctx.cause.entity
         if (player !is ServerPlayer) return
 
-        val pokemon = spawnEvent.entity.pokemon
+        val pokemon = evt.entity.pokemon
         player.record(pokemon, CounterTypes.FISH)
     }
-
 }
